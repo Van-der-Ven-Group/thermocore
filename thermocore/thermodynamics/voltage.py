14e2b2f4797147414d91b698521559d500213271
Muna Saber <munasaber@ucsb.edu>
2022-05-05 14:01:45 -0700

    Ran voltage.py through python black

--- thermocore/thermodynamics/voltage.py
+++ thermocore/thermodynamics/voltage.py
@@ -2,17 +2,19 @@ from __future__ import annotations
 import numpy as np
 
 
-def intercalation_chemical_potential(data: dict, number_of_intercalant_per_unit_cell: int)-> np.ndarray:
+def intercalation_chemical_potential(
+    data: dict, number_of_intercalant_per_unit_cell: int
+) -> np.ndarray:
     """
     This function calculates the chemical potential using DFT predicted values for the formation energies and corresponding compositions (placed in the form of a dictionary)
-    
+
     Arguments
     ---
     data : dict
         A dictionary of crystallogrpahic data that contains the DFT predicted formation energies of differing intercalant-vacancy orderings and corresponding intercalant compositions.
     number_of_intercalant_per_unit_cell : int
-        The number of intercalants that are degrees of freedom in the system (generally the number of available intercalant sites). 
-    
+        The number of intercalants that are degrees of freedom in the system (generally the number of available intercalant sites).
+
     Returns
     ---
     chemical_potential : np.ndarray
@@ -20,42 +22,54 @@ def intercalation_chemical_potential(data: dict, number_of_intercalant_per_unit_
     """
 
 
-def intercalation_voltage(data: dict,  refernce_chemical_potential:float, number_of_intercalant_per_unit_cell:int, electrons_per_species: int)-> np.ndarray, np.nndarry:
+def intercalation_voltage(
+    data: dict,
+    refernce_chemical_potential: float,
+    number_of_intercalant_per_unit_cell: int,
+    electrons_per_species: int,
+) -> np.ndarray:
     """
     This function calculates voltage from a chemical_potential matrix using the Nernst equation: V=-(\mu-\mu^{0})/(ne) where:
         \mu is the chemical_potential.
         \mu^{0} is the chemical potential for the reference.
         n is the number of electrons_per_species.
         e is accouted for in VASP determined calculations (which are in eV) and are ignored in the usage of this function.
-    
+
     Arguments
     ---
     data : dict
         A dictionary of crystallogrpahic data that contains the DFT predicted formation energies of differing intercalant-vacancy orderings and corresponding intercalant compositions.
-    reference_chemical_potential : float 
+    reference_chemical_potential : float
         The chemical potential of the chemical reference in the Nernset equation (in eV)
     number_of_intercalant_per_unit_cell : int
         The number of potential intercalant ion sites present per unit cell.
     electrons_per_species : int
         Number of electrons released for each intercalant (e.g. 1 Li ion will release 1 e-)
-    
+
     Returns
     ---
     voltage : np.ndarray
-        Voltage as calculated from the input chemical_potential and reference chemical_potential. 
+        Voltage as calculated from the input chemical_potential and reference chemical_potential.
     voltage_corresponding_compositions : np.ndarray
-        Compositions for each determined vertex of the voltage steps. 
+        Compositions for each determined vertex of the voltage steps.
     """
 
 
-def GCMC_intercalation_voltage(GCMC_data: dict, refernce_chemical_potential:float, free_energy_reference_startstate:float, free_energy_reference_endstate: float, number_of_intercalant_per_unit_cell:int, electrons_per_species: int)-> np.ndarray, np.ndarray:
+def GCMC_intercalation_voltage(
+    GCMC_data: dict,
+    refernce_chemical_potential: float,
+    free_energy_reference_startstate: float,
+    free_energy_reference_endstate: float,
+    number_of_intercalant_per_unit_cell: int,
+    electrons_per_species: int,
+) -> np.ndarray:
     """
     This function calculates voltage from a chemical_potential matrix using the Nernst equation: V=-(\mu-\mu^{0})/(ne) where:
         \mu is the chemical_potential.
         \mu^{0} is the chemical potential for the reference.
         n is the number of electrons_per_species.
         e is accouted for in VASP determined calculations (which are in eV) and are ignored in the usage of this function.
-    
+
     Arguments
     ---
     GCMC_data : dict
@@ -70,11 +84,11 @@ def GCMC_intercalation_voltage(GCMC_data: dict, refernce_chemical_potential:floa
         The number of potential intercalant ion sites present per unit cell.
     electrons_per_species : int
         Number of electrons released for each intercalant (e.g. 1 Li ion will release 1 e-)
-        
+
     Returns
     ---
     voltage : np.ndarray
-        Voltage as calculated from the input chemical_potential and reference chemical_potential. 
+        Voltage as calculated from the input chemical_potential and reference chemical_potential.
     voltage_corresponding_compositions : np.ndarray
-        Compositions for each determined vertex of the voltage steps. 
+        Compositions for each determined vertex of the voltage steps.
     """
