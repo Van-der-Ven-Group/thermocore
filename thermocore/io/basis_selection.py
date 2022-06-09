e289835dee3353ab76a85538e6b71a0fbd177256
jonnyli <jli5373@gmail.com>
2022-06-08 18:05:24 -0700

    updated implementation of basis_selection. filters now return None (thus not applied) if the input is irrelevant

--- thermocore/io/basis_selection.py
+++ thermocore/io/basis_selection.py
@@ -1,6 +1,7 @@
 from collections.abc import Sequence
 from typing import Callable
 import numpy as np
+from sklearn import cluster
 
 
 def select_basis_functions_by_orbit_filters(
@@ -13,24 +14,35 @@ def select_basis_functions_by_orbit_filters(
     orbit_filters : Sequence of functions
         Boolean functions operating on individual orbit data to use as selection criteria.
         Each function must take a dict of one orbits's data (e.g. one element of `basis_data`["orbits"])
-        as its sole parameter return a bool indicating whether the orbit meets that particular criterion.
+        as its sole parameter and returns a bool indicating whether the orbit meets that particular criterion.
+            If the orbit does not meet the criterion, the orbit is excluded from the selection.
+            If the orbit cannot be filtered by the function (ie. different cluster_size for length filters),
+            the filter returns None and is ignored.
     basis_data : dict
         CASM basis set data (e.g. loaded from a basis.json file).
 
     Returns
     -------
     selected_indices : list[int]
-        Linear function indices of basis functions that meet all of the selection criteria.
+        Linear function indices of basis functions that meet all of the selection criteria (ignores None).
 
     Example usage
     -------------
     # Pass filters to select_basis_functions_by_orbit_filters
-    selection_indices = select_basis_functions_by_orbit_filters([max_length_filter(4.5,2), max_length_filter(4.0,3)], basis_data)
+    selection_indices = select_basis_functions_by_orbit_filters([max_length_filter(4.5,2),
+                                                                max_length_filter(4.0,3)],
+                                                                basis_data)
     ------------------------------------------------------------------
     """
     selected_indices = []
     for orbit in basis_data["orbits"]:
-        if all([selection(orbit) for selection in orbit_filters]):
+        if all(
+            [
+                x
+                for x in [selection(orbit) for selection in orbit_filters]
+                if x is not None
+            ]
+        ):
             selected_indices.extend(
                 [
                     cluster["linear_function_index"]
@@ -41,7 +53,14 @@ def select_basis_functions_by_orbit_filters(
 
 
 def max_length_filter(cluster_size: int, max_length: float) -> Callable:
-    """Returns function that takes orbit_data as input for the selection filtering. Returned function will return False if a given orbit of size cluster_size has maximum cluster size > max_length. Returns True otherwise.
+    """Returns a function that takes orbit_data as input for the selection filtering.
+    The returned function returns False if an orbit of size cluster_size
+        has maximum cluster size > max_length.
+    Function returns True if the orbit of size cluster_size
+        has maximum cluster size <= max_length.
+    Function returns None if the orbit is not of size cluster_size
+        (aka this filter cannot be applied)
+
 
     Parameters
     ----------
@@ -53,12 +72,18 @@ def max_length_filter(cluster_size: int, max_length: float) -> Callable:
     Returns
     -------
     sub_max_length_filter : function
-        Function takes 1 argument: a dictionary of orbit data. The function will return a bool based on the input criteria.
+        Function takes 1 argument: a dictionary of orbit data.
+        The function will return a bool (or None) based on the input criteria.
     """
 
     def sub_max_length_filter(orbit_data: dict) -> bool:
         """Sub function for max_length_filter.
-        Returns False if the orbit in `orbit_data` has `cluster_size` points and a maximum distance between points greater than `maximum_length`, returns True otherwise.
+        Returns False if the orbit in `orbit_data` has `cluster_size` points
+            and a maximum distance between points greater than `maximum_length`
+        Returns True if the orbit in `orbit_data` has `cluster_size` points
+            and a maximum distance between points less than `maximum_length`
+        Returns None if the orbit is not of size cluster_size
+            (aka this filter cannot be applied)
 
         Parameters
         ----------
@@ -70,16 +95,22 @@ def max_length_filter(cluster_size: int, max_length: float) -> Callable:
         bool
             Whether the orbit passes the maximum length filter.
         """
-        return (
-            orbit_data["prototype"]["max_length"] <= max_length
-            and len(orbit_data["prototype"]["sites"]) == cluster_size
-        ) or len(orbit_data["prototype"]["sites"]) != cluster_size
+        if len(orbit_data["prototype"]["sites"]) != cluster_size:
+            return None
+        else:
+            return orbit_data["prototype"]["max_length"] <= max_length
 
     return sub_max_length_filter
 
 
 def min_length_filter(cluster_size: int, min_length: float) -> Callable:
-    """Returns function that takes orbit_data as input for the selection filtering. Returned function will return False if a given orbit of size cluster_size has minimum cluster size < min_length. Returns True otherwise.
+    """Returns function that takes orbit_data as input for the selection filtering.
+    Returned function will return False if orbit of size cluster_size
+        has minimum cluster size < min_length.
+    Returned function will True if orbit of size cluster_size
+        has minimum cluster size > min_length.
+    Returned function will None if orbit is not of size cluster_size
+        (aka this filter cannot be applied).
 
     Parameters
     ----------
@@ -91,12 +122,18 @@ def min_length_filter(cluster_size: int, min_length: float) -> Callable:
     Returns
     -------
     sub_min_length_filter : function
-        Function takes 1 argument: a dictionary of orbit data. The function will return a bool based on the input criteria.
+        Function takes 1 argument: a dictionary of orbit data.
+        The function will return a bool (or None) based on the input criteria.
     """
 
     def sub_min_length_filter(orbit_data: dict) -> bool:
         """Sub function for max_length_filter.
-        Returns False if the orbit in `orbit_data` has `cluster_size` points and a minimum distance between points less than `min_length`, returns True otherwise.
+        Returns False if the orbit in `orbit_data` has `cluster_size` points
+            and a minimum distance between points less than `min_length`.
+        Returns True if the orbit in `orbit_data` has `cluster_size` points
+            and a minimum distance between points greater than `min_length`.
+        Returns None if the orbit is not of size cluster_size
+            (aka this filter cannot be applied).
 
         Parameters
         ----------
@@ -108,9 +145,9 @@ def min_length_filter(cluster_size: int, min_length: float) -> Callable:
         bool
             Whether the orbit passes the minimum length filter.
         """
-        return (
-            orbit_data["prototype"]["min_length"] >= min_length
-            and len(orbit_data["prototype"]["sites"]) == cluster_size
-        ) or len(orbit_data["prototype"]["sites"]) != cluster_size
+        if len(orbit_data["prototype"]["sites"]) != cluster_size:
+            return None
+        else:
+            return orbit_data["prototype"]["min_length"] >= min_length
 
     return sub_min_length_filter
